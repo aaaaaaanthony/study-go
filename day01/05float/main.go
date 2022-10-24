1cf88a162fbf1c1c81f4e662ebb7bd4985f77edb
OldYoung <mmzcg@outlook.com>
2022-10-24 15:49:53 +0800

    1

--- day01/05float/main.go
+++ day01/05float/main.go
@@ -6,8 +6,11 @@ import (
 
 // 浮点数
 func main() {
-
+	// math.MaxFloat32   float32最大值
 	f1 := 1.23456
+	fmt.Printf("%T\n", f1) // go默认小数都是float64类型
+	f2 := float32(1.23456)
+	fmt.Printf("%T\n", f2)
 
-	fmt.Printf("%T\n", f1)
+	// f1= f2  float32类型的值不能直接转
 }
