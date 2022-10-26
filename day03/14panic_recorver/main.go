0a017149788791439cbdb2645ff81ee28a8aee6f
OldYoung <mmzcg@outlook.com>
2022-10-26 17:01:27 +0800

    1

---
+++ day03/14panic_recorver/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	funcA()
+	funcB()
+	funcC()
+}
+
+func funcA() {
+
+	fmt.Println("a")
+}
+
+func funcB() {
+
+	// 刚打开数据库链接
+	defer func() {
+		err := recover()
+		fmt.Println(err)
+		fmt.Println("释放数据库")
+	}()
+
+	// 程序崩溃退出
+	panic("出现了严重的错误")
+	fmt.Println("b")
+}
+
+func funcC() {
+	fmt.Println("c")
+}
---
+++ day03/15fmt/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 获取用户输入
+	var s string
+	fmt.Scan(&s)
+	fmt.Println(s)
+
+	fmt.Scanf("",&s)
+
+}
